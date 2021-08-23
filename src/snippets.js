a9780f103c9d9210e7fdd0a5832bbe3b761451e0
Giu Zambot <giuzambot@gmail.com>
2021-08-23 11:52:10 -0300

    Atualização não essencial, só para backup

--- src/snippets.js
+++ src/snippets.js
@@ -1,17 +1,21 @@
 const snippets = [
     "console.log('teste');",
-    "console.log(5 + 7);",
-    "console.log(5 + 7);",
-    "console.log(5 + 7);",
+    "console.log(5 * 7);",
+    "console.log(5 / 7);",
+    "console.log(5 ** 2);",
     "const a = 5 \nconsole.log(a + 7);",
 ];
 
 snippets.push(`
-    console.log(5 + 7);
+    console.log(25 ** 0.5);
 `);
 
 snippets.push(`
     console.log('teste');
+`);
+
+snippets.push(`
+    console.log('start tech teste');
 `);
 
 module.exports = { snippets };
