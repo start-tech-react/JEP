b21c746fde782c812eef1649c665f45b75a2214a
Giu Zambot <giuzambot@gmail.com>
2021-08-25 20:38:43 -0300

    Alterações para teste Vercel

--- src/snippets/snippets.js
+++ src/snippets/snippets.js
@@ -5,21 +5,4 @@ for (let i = 1; i <= 2; i++) {
     snippets.push(...snippetsCap);
 }
 
-const categories = [
-    'Cap.1',
-    'Cap.1 Tipos',
-    'Cap.1 Operadores Aritiméticos',
-    'Cap.1 Operações com Strings',
-    'Cap.1 Booleanos Valores Truthy',
-    'Cap.1 Booleanos Valores Falsy',
-    'Cap.1 Comparações',
-    'Cap.1 Resumo',
-    'Cap.2 Escopos',
-    'Cap.2 Funções',
-    'Cap.2 Execução Condicional',
-    'Cap.2 Condicional Switch',
-    'Cap.2 Loops',
-    'Cap.2 Atualização Sucinta'
-];
-
-module.exports = { categories, snippets };
+module.exports = { snippets };
