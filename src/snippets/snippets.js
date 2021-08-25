4692ab25aa8982e6e5ce5002561bb50046ef3af4
Giu Zambot <giuzambot@gmail.com>
2021-08-25 20:57:49 -0300

    Alterações para teste Vercel

--- src/snippets/snippets.js
+++ src/snippets/snippets.js
@@ -1,8 +1,8 @@
 // Capítulos
 const snippets = [];
-const { snippetsCap: cap1 } = require("./cap1");
-snippets.push(...cap1);
-const { snippetsCap: cap2 } = require("./cap2");
-snippets.push(...cap2);
+const { snippetsCap } = require("./cap1");
+snippets.push(...snippetsCap);
+//const { snippetsCap: cap2 } = require("./cap2");
+//snippets.push(...require("./cap2"));
 
 module.exports = { snippets };
